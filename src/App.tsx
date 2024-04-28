f3d5bda5c8b04b8110a381e0dd4a7cc47c5ef3cf
willamessantos <willamessantos@live.com>
2024-04-28 15:09:03 -0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/defualt";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hellor world!</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
