334f434cff9246d7649d22c7645d6824a3cb68b9
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2024-04-29 14:26:48 -0300

    feat: create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/defualt'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hellor world!</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
