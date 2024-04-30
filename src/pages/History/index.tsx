8b0af7571a208b568ff1ad041517a0713e7cc229
willamessantos <willamessantos@live.com>
2024-04-29 23:32:15 -0300

    feat: create history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,54 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Nome da Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerda de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Nome da Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerda de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Nome da Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerda de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Nome da Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerda de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Nome da Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerda de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
